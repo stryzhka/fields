07c236669470b4a76a686de9d2a344f33673508a
doomguyfromrussia <voditel.vlad@yandex.ru>
2020-02-09 14:23:39 +0300

    изменил отображение элементов интерфейса теперь хп увеличивается если игрок получает урон

--- resources/clientScripts/uiUpdater.cs
+++ resources/clientScripts/uiUpdater.cs
@@ -11,11 +11,11 @@ public class uiUpdater : MonoBehaviour
     public Text levelText;
     public Text expText;
     public Text expReqText;
-
+    public bool doRotating;
  
     void Start()
     {
-        
+        doRotating = false;
     }
 
     // Update is called once per frame
@@ -27,5 +27,12 @@ public class uiUpdater : MonoBehaviour
        levelText.text = "Level: " +  playerStats.level; 
        expText.text = "XP: " + playerStats.exp.ToString(); 
        expReqText.text = "XP need: " + playerStats.expReq.ToString(); 
+       if (doRotating) rotate();
+       else hpText.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+    public void rotate(){
+      
+      if (hpText.gameObject.transform.localScale.magnitude > 10) hpText.gameObject.transform.localScale = new Vector3(0.0f, 0f, 0.0f);
+      else hpText.gameObject.transform.localScale += new Vector3(2f * Time.deltaTime, 2f * Time.deltaTime, 0.0f);
     }
 }
