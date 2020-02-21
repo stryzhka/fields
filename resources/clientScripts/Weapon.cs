39a774db870535074e088b68a4ac33e8740ee2b4
doomguyfromrussia <voditel.vlad@yandex.ru>
2020-02-21 21:14:42 +0300

    новые поля: тип и точность оружия (для дальнобойного)

--- resources/clientScripts/Weapon.cs
+++ resources/clientScripts/Weapon.cs
@@ -10,16 +10,20 @@ public class Weapon : Item
     public string effect;
     public int effectChance;
     public int effectDamage;
-    public Weapon(string n, string p, string ip, int pr, int bD, float d, string s, int ec, int ed, string dP) :base(n, p, pr, ip, dP){
+    public string type;
+    public int accuracy;
+    public Weapon(string t, int ac, string n, string p, string ip, int pr, int bD, float d, string s, int ec, int ed, string dP) :base(n, p, pr, ip, dP){
 
     	this.baseDamage = bD;
     	this.effect = s;
     	this.delay = d;
     	this.effectChance = ec;
     	this.effectDamage = ed;
+        this.type = t;
+        this.accuracy = ac;
     }
     public string info(){
-    	return "Name: " + name + " Damage: " + baseDamage + " Effect: " + effect + " EffectDamage: " + effectDamage + " EffectChance: " + effectChance + " Delay: " + delay;
+    	return "Type: " + type + "Accuracy: " + accuracy +"Name: " + name + " Damage: " + baseDamage + " Effect: " + effect + " EffectDamage: " + effectDamage + " EffectChance: " + effectChance + " Delay: " + delay;
     }
     public string descInfo(){
         return "\nDamage: " + baseDamage + " Effect: " + effect + " EffectDamage: " + effectDamage + " EffectChance: " + effectChance + " Delay: " + delay;
