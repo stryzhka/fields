90c828ada468a2381dfed8a7d7f13c0b0adea074
doomguyfromrussia <voditel.vlad@yandex.ru>
2020-02-28 16:15:35 +0300

    add current pos loading

--- resources/clientScripts/waypointScript.cs
+++ resources/clientScripts/waypointScript.cs
@@ -6,15 +6,39 @@ using UnityEngine.SceneManagement;
 public class waypointScript : MonoBehaviour
 {
     public string scene;
+    public float x, y, z;
+    public bool loadPos;
+    public bool set;
     void Start()
     {
-        
+        if (PlayerPrefs.GetInt("load") == 1 && set)
+        GameObject.Find("player").transform.position = new Vector3(x, y, z);
+        else print ("аутизм");
+        print(PlayerPrefs.GetString("currentWay"));
+        print(loadPos);
     }
 
     void OnCollisionEnter2D(Collision2D col){
         if (col.gameObject.tag == "Player"){
         	playerStats.setAllData();
+            PlayerPrefs.SetString("currentWay", gameObject.name);
+            PlayerPrefs.SetFloat("x", x);
+            PlayerPrefs.SetFloat("y", x);
+            PlayerPrefs.SetFloat("z", x);
+            if (loadPos)
+            PlayerPrefs.SetInt("load", 1);
+            else
+            PlayerPrefs.SetInt("load", 0);
+
+            if (scene == "city2"){
+                PlayerPrefs.SetString("city", "city2");
+                print ("ok");
+                print (PlayerPrefs.GetString("city"));
+            }
+            
         	SceneManager.LoadScene(scene, LoadSceneMode.Single);
+            
+            
         }
             
     }
