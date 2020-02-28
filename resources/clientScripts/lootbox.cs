36e6a3fa57bbf714ddf09c72e58f63862025d292
doomguyfromrussia <voditel.vlad@yandex.ru>
2020-02-28 16:14:56 +0300

    add weapon spawning

--- resources/clientScripts/lootbox.cs
+++ resources/clientScripts/lootbox.cs
@@ -7,6 +7,7 @@ public class lootbox : MonoBehaviour
     public string listPath;
     public GameObject box;
     public GameObject player;
+    public RectTransform weapons;
     void Start()
     {
         
@@ -15,17 +16,18 @@ public class lootbox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("s")){
+            loader loader = new loader();
+            string weaponPath = loader.loadList(listPath);
+            GameObject _box = Instantiate(box, transform.position, Quaternion.identity);
+            _box.GetComponent<clickable>().weaponPath = weaponPath;
+            _box.name = "weapon";
+            _box.GetComponent<clickable>().weapons = weapons;
+            player.GetComponent<inventoryManager>().isActive = true;
+        }
     }
     void OnMouseDown(){
-    	if (Vector2.Distance(transform.position, player.transform.position) <= 2){
-    		loader loader = new loader();
-	        string weaponPath = loader.loadList(listPath);
-	        GameObject _box = Instantiate(box, player.transform.position, Quaternion.identity);
-	        _box.GetComponent<clickable>().weaponPath = weaponPath;
-            _box.name = "weapon";
-            player.GetComponent<inventoryManager>().isActive = true;
-            Destroy (gameObject);
-    	}
+    		
+            //Destroy (gameObject);
     }
 }
